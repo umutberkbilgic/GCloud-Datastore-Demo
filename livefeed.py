726b311386da7336f6e576231b53325c219fc10a
Umut Berk Bilgiç <umutbilgicapple@gmail.com>
2017-07-25 09:37:15 +0300

    Create livefeed.py
    Console is not a good place for this. It is not really a live feed if it is waiting for 4 seconds for very fetch. GCloud connection is a bottleneck. GUI this up with tkinter.

---
+++ livefeed.py
@@ -0,0 +1,64 @@
+
+# Umut Berk Bilgic
+# July 2017
+# @ Sebit Information & Education Technologies
+# METU Teknokent, Ankara, Turkey
+
+from google.cloud import datastore
+import time
+import os
+
+client = datastore.Client()
+
+# Main loop 
+while True:
+	# info
+	print("[1]: Livefeeed, [0]: Exit")
+	prompt = raw_input("\n>> ")
+	print("")
+
+	if (prompt == "1" or prompt.lower() == "livefeed"):
+		
+		while (True):
+			
+			
+			c = datastore.Client()
+			kind = "comments"
+
+			query = c.query(kind = "comments")
+			query.add_filter("userid", ">", 0)
+
+			result_list = list(query.fetch())
+
+			comment_id_list = []
+
+			for r in result_list:
+				r = str(r)
+				index_start = r.find("u'comments', ") + 13
+				index_end = r.find("L", index_start)
+				comment_id_list.append(int(r[index_start : index_end]))
+
+			os.system("clear")
+			
+			for id in comment_id_list:	
+				comment_key = c.key(kind, id)
+				ent = client.get(comment_key)
+
+				comment = ent.get("comment")
+				username = ent.get("username")
+				
+				
+
+				print ("Comment by " + username + ": " + comment + "\n")
+				
+			time.sleep(4)
+		
+
+	elif (prompt == "0" or prompt.lower() == "exit"):
+		print("Exiting...\n")
+		exit()
+
+	else:
+		print("\nInvalid input.\n")
+		
+		
