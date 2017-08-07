6e8e09694041764e188e1690550b22de53d4c4f4
Umut Berk Bilgiç <umutbilgicapple@gmail.com>
2017-08-07 14:31:15 +0300

    Update cars_script.py
    Nah, forget about try catches will add em later

--- cars_script.py
+++ cars_script.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 import cgi
 from google.cloud import datastore
  
@@ -15,34 +14,17 @@ search_color = form.getvalue("f_color")
 search_price = form.getvalue("f_price")
 search_power = form.getvalue("f_power")
  
-try:
-        search_price_final = int(search_price)
-except ValueError:
-        search_price_final = 0
- 
-try:
-        search_power_final = int(search_power)
-except ValueError:
-        search_power_final = 0
- 
-try:
-        search_year_final = int(search_year)
-except ValueError:
-        search_year_final = 0
- 
-search_make_final = unicode(search_make)
-search_model_final = unicode(search_model)
-search_color_final = unicode(search_color)
- 
-features = {"make"  : search_make_final,
-            "model" : search_model_final,
-            "year"  : search_year_final,
-            "color" : search_color_final,
-            "price" : search_price_final,
-            "power" : search_power_final}
+features = {"make"  : unicode(search_make),
+            "model" : unicode(search_model),
+            "year"  : int(search_year),
+            "color" : unicode(search_color),
+            "price" : int(search_price),
+            "power" : int(search_power)}
 car.update(features)
  
 client.put(car)
+ 
+format_price = ("{:,}".format(int(search_price)))
  
 print "Content-type:text/html\r\n\r\n"
 print "<html>"
@@ -50,15 +32,19 @@ print "<head>"
 print "<title>Car</title>"
 print "</head>"
 print "<body>"
-print "<h2> Make: "  + search_make_final  + "</h2>"
-print "<h2> Model: " + search_model_final + "</h2>"
-print "<h2> Year: "  + search_year_final  + "</h2>"
-print "<h2> Color: " + search_color_final + "</h2>"
-print "<h2> Price: $ " + search_price_final + "</h2>"
-print "<h2> Power: "   + search_power_final + " HP </h2>"
+print "<h2> Make: "    + search_make  + "</h2>"
+print "<h2> Model: "   + search_model + "</h2>"
+print "<h2> Year: "    + search_year  + "</h2>"
+print "<h2> Color: "   + search_color + "</h2>"
+print "<h2> Price: $ " + format_price + "</h2>"
+print "<h2> Power: "   + search_power + " HP </h2>"
  
 print "<form action=\"http://35.195.58.189/\">"
 print "<input type=\"submit\" value=\"Go Back\" />"
+print "</form>"
+ 
+print "<form action=\"/cgi-bin/cars.py\">"
+print "<input type=\"submit\" value=\"View cars\" />"
 print "</form>"
  
 print "</body>"
